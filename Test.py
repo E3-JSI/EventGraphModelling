206abcb201cf47515eb075f0ec5d42efe9825a33
Miha Torkar <miha.torkar0@gmail.com>
2019-06-06 14:28:57 +0200

    Incorporated iteration through time

--- Test.py
+++ Test.py
@@ -10,55 +10,51 @@ import matplotlib.pyplot as plt
 
 # Parameters
 p = 0.5
-dt_max=200
+subset = 100
+const = 60*60*24*7 # 1 week
 
-#if False:
-    #T = 100
-    #network_hypers = {"p": p, "allow_self_connections": False}
-
-    #true_model = DiscreteTimeNetworkHawkesModelSpikeAndSlab(
-        #K=K, dt_max=dt_max,
-        #network_hypers=network_hypers)
-    #assert true_model.check_stability()
-
-    # Synthetic data
-    #S,R = true_model.generate(T=T, keep=True, print_interval=50)
-
-# Real data
+# Load settings file
 settings = json.load(open("settings.json","r"))
 # Path to file
 events_embd = settings["out_path"]+"concepts_date_embd.csv"
-# load events
+# Load events
 print("Reading in data")
 events = pd.read_csv(events_embd,header=None)
-subset = 200
+# Timestamps of events in UNIX format
+time = np.unique(events.iloc[:,-1].values)
 
-L = events.head(subset).iloc[:,:-1].values
+# Iterate through data
+for t in time: 
+    t_start = t - const 
+    # Get only events in the time period
+    tmp=events[(events.iloc[:,-1]<=t) & (events.iloc[:,-1]>=t_start)]
+    # Take a subset, ideally we could take all
+    L = tmp.head(subset).iloc[:,:-1].values
+    print("Calculating W from data")
+    W_real = er.get_W(L)
 
-W_real = er.get_W(L)
+    # Set to minimum of limit and shape of data (due to starting period with less events)
+    K = min(subset,tmp.shape[0])
+    S_real = np.eye(K,dtype=np.int)
 
-S_real = np.eye(subset,dtype=np.int)#events.head(subset)[300].values
-#S_real = [[itm] for _,itm in Counter(S_real).items()]
-K = subset
-# Define network
-ld_network = er.LatentDistanceAdjacencyModel(K=K, L = None, dim=2, v=None, alpha=1.0, beta=1.0,kappa=1.0,p = p)
+    # Define network
+    ld_network = er.LatentDistanceAdjacencyModel(K=K, L = None, dim=2, v=None, alpha=1.0, beta=1.0,kappa=1.0,p = p)
 
-# resample graph with new data 
-ld_network.resample(data=[S_real,W_real])
+    print("Resampling ld network")
+    ld_network.resample(data=[S_real,W_real])
 
-# Define weight model
-weight_model = er.SpikeAndSlabGammaWeights(model = ld_network, parallel_resampling=False)
+    # Define weight model
+    weight_model = er.SpikeAndSlabGammaWeights(model = ld_network, parallel_resampling=False)
 
-# Add data
-weight_model.resample_new(data=[S_real,W_real])
+    print("Resampling weight model")
+    weight_model.resample_new(data=[S_real,W_real])
 
-# test_model = DiscreteTimeNetworkHawkesModelSpikeAndSlab(K=K, dt_max=dt_max,network=ld_network)
+    # Print adjecancy matrix and it's properties 
+    print(weight_model.A)
+    print(sum(weight_model.A))
+    print(max(sum(weight_model.A)))
 
-# test_model.add_data(S_real)
-# test_model.resample_model()
-
-# test_figure, test_handles = test_model.plot(color="#e41a1c", T_slice=(0,subset))
-# plt.show()
-G = nx.from_numpy_matrix(weight_model.A)
-nx.draw(G,with_labels=True,with_edges=True)
-plt.show()
+    # Plot graph
+    G = nx.from_numpy_matrix(weight_model.A)
+    nx.draw(G,with_labels=True,with_edges=True)
+    plt.show()
